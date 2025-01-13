674f00df43a69d4a2c386825d1ebfd28402b7af3
Qurbani Amrakhov <qurbaniemrah@outlook.com>
2025-01-14 03:43:50 +0400

    working on structure

---
+++ Admin/resources/views/partials/_footer.php
@@ -0,0 +1,20 @@
+</div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../resources/assets/js/off-canvas.js"></script>
+    <script src="../resources/assets/js/hoverable-collapse.js"></script>
+    <script src="../resources/assets/js/misc.js"></script>
+    <script src="../resources/assets/js/settings.js"></script>
+    <script src="../resources/assets/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
+  </body>
+</html>
---
+++ Admin/resources/views/partials/_main_panel.php
@@ -0,0 +1,79 @@
+<div class="main-panel">
+          <div class="content-wrapper">
+           
+            <div class="page-header">
+
+              <h3 class="page-title">İstifadəçilər</h3>
+           <a href="./components/add.html">
+            <button class="btn btn-info">
+              Add
+            </button>
+           </a>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  
+                 
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+              <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                   
+                    </p>
+                    <div class="table-responsive">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                         
+                            <th>Email</th>
+                            <th>Şifrə</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                         
+                            <td>test@mail.ru</td>
+                            <td>1234</td>
+                            <td>
+                              <a class="btn btn-success" href="../partials/_edit.html">
+                                Edit
+                              </a>
+                            </td>
+                            <td>
+                                <a class="btn btn-danger" href="">
+                                  Delete
+                                </a>
+                              </td>
+                          </tr>
+                         
+                        
+                        
+                        
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+             
+             
+             
+             
+           
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:../../partials/_footer.html -->
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
