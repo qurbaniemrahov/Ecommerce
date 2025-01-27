0988c4ba4bf37e8d2d2ef53c299f763d261f11cd
Qurbani Amrakhov <qurbaniemrah@outlook.com>
2025-01-27 22:07:42 +0400

    updated

--- Admin/app/Http/Controllers/user/user.php
+++ Admin/app/Http/Controllers/user/user.php
@@ -1,6 +1,24 @@
-<?php 
+<?php
 include("../../../../config/connection.php");
 
+try {
+    // Prepare the SQL statement
+    $stmt = $pdo->prepare("INSERT INTO admin_user (email, password) 
+                           VALUES (:email, :password)");
 
+    // Execute the statement with provided parameters
+    $stmt->execute([
+        "email" => $_POST["email"],
+        "password" => password_hash($_POST["password"], PASSWORD_DEFAULT), // Hash the password for security
+    ]);
+
+    echo "User added successfully.";
+} catch (PDOException $e) {
+    // Handle any errors
+    echo "Error: " . $e->getMessage();
+}
 ?>
+
+
+
 
