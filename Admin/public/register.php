2bbfe79c4a60b13be95bca1caf082b1d93ca4f6f
qurbaniemrahov <qurbaniemrah@outlook.com>
2025-11-25 20:55:59 +0400

    updated

--- Admin/public/register.php
+++ Admin/public/register.php
@@ -1,7 +1,7 @@
 <?php 
 include("../resources/views/partials/_header.php");
 include("../resources/views/partials/_sidebar.php");
-include("../resources/views/partials/_main_panel.php");
+include("../resources/views/partials/_main_panel_register.php");
 include("../resources/views/partials/_footer.php");
 
 ?> 
---
+++ Admin/resources/views/partials/_main_panel_register.php
@@ -0,0 +1,65 @@
+<div class="main-panel">
+          <div class="content-wrapper">
+           
+            <div class="page-header">
+
+              <h3 class="page-title">İstifadəçilər</h3>
+           <a href="../resources/views/components/_user_add.php">
+            <button class="btn btn-info">
+              Add
+            </button>
+           </a>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  
+                 
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+              <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                   
+                    </p>
+                    <div class="table-responsive">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                          <th>Id</th>
+                            <th>Email</th>
+                            <!-- <th>Şifrə</th> -->
+                            <th>Edit</th>
+                            <th>Delete</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                         
+
+
+
+
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+             
+             
+             
+             
+           
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:../../partials/_footer.html -->
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
